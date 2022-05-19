a4f3eaaaf1da20e58bf794bbdd1d8fe2e1646e97
Salamon <ibrohim238@mail.ru>
2022-05-20 01:23:34 +0300

    composer require swagger, add parameter swagger in controller, config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Foundation documentation",
+ *         description="Demo Todo List Api",
+ *     )
+ * )
+ * @OA\Tag(
+ *      name="Cars",
+ *      description="Car Pages",
+ * )
+ * @OA\Server(
+ *     description="Laravel swagger API server",
+ *     url="http://localhost:8035/api",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- app/Http/Requests/CarRequest.php
+++ app/Http/Requests/CarRequest.php
@@ -8,6 +8,11 @@ use Illuminate\Foundation\Http\FormRequest;
 */
 class CarRequest extends FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
     public function rules(): array
     {
         return [
